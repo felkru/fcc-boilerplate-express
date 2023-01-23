84f6af578e7809c1975658009da6827a3b691a36
Felix Krückel <64913698+felkru@users.noreply.github.com>
2023-01-23 11:50:00 +0000

    added contents of first few challenges

--- myApp.js
+++ myApp.js
@@ -1,39 +1,14 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+let obj = { message: "Hello json" };
 
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
+app.get("/json", function (req, res) {
+  if (process.env["MESSAGE_STYLE"] == "uppercase") {
+    obj.message = obj.message.toUpperCase();
+  }
+  res.json(obj);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
