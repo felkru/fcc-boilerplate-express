c460e0d005a8b8b057ba535b50d4ae91d174595d
Felix Krückel <64913698+felkru@users.noreply.github.com>
2023-01-23 18:53:27 +0000

    successfully finished all challenges

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
+let bodyParser = require("body-parser");
 let express = require("express");
 let app = express();
 let obj = { message: "Hello json" };
 
-app.use("/", function (req, res, next) {
-  console.log(req.method + " " + req.path + " - " + req.ip);
-  next();
-});
+app.use(
+  "/",
+  function (req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+  },
+  bodyParser.urlencoded({ extended: false })
+);
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.get("/json", function (req, res) {
@@ -14,5 +19,26 @@ app.get("/json", function (req, res) {
   }
   res.json(obj);
 });
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
+app.get("/:word/echo", function (req, res) {
+  res.json({ echo: req.params.word.toString() });
+});
+app
+  .route("/name")
+  .get(function (req, res) {
+    res.json({ name: req.query.first + " " + req.query.last });
+  })
+  .post(function (req, res) {
+    res.json({ name: req.body.first + " " + req.body.last });
+  });
 
 module.exports = app;
